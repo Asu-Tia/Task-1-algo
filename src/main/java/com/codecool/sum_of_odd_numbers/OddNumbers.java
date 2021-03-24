aaab093e3594ff0a67507d351675841dffd2c597
Gergő Csongor Vincze <65530422+Asu-Tia@users.noreply.github.com>
2021-03-24 15:07:28 +0100

    Task complete.

--- src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
+++ src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
@@ -5,6 +5,12 @@ import java.util.List;
 public class OddNumbers {
 
     public static Integer sum(List<Integer> numbers) {
-        return null;
+        int sum = 0;
+        for (Integer number : numbers) {
+            if (Math.abs(number) % 2 == 1) {
+                sum += number;
+            }
+        }
+        return sum;
     }
 }
